d22c2301853716a5ccd09339d819a3ed56ed3459
Shoo <snakexxs80@gmail.com>
2026-02-24 22:38:51 -0600

    Pto scroll.

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -165,7 +165,7 @@ export default function HeroSection() {
 
             {/* Scroll indicator */}
             <motion.div
-                className="absolute bottom-14 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
+                className="scroll-indicator absolute bottom-14 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 1.4, duration: 0.6 }}
